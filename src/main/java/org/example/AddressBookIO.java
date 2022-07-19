a2595c6207cc95a3658048de1fd146113cb7f2f8
Inamul <inamulhoque097@gmail.com>
2022-07-19 21:39:30 +0530

    Added: Ability to read and write from and to Json file.

--- src/main/java/org/example/AddressBookIO.java
+++ src/main/java/org/example/AddressBookIO.java
@@ -1,40 +1,26 @@
 package org.example;
 
 import com.google.gson.Gson;
-import com.opencsv.CSVWriter;
-import com.sun.tools.jdeprscan.CSV;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class AddressBookIO {
-    static CSV csv = new CSV();
+    static Gson gson = new Gson();
     static File file = new File("AddressBook.csv");
     public static void writeIntoFile() throws IOException{
+        String str = gson.toJson(AddressBookFunctions.hashMapOfAddressBook);
+        file.createNewFile();
         FileWriter fileWriter = new FileWriter(file);
-        CSVWriter csvWriter = new CSVWriter(fileWriter);
-        String[] strContacts = new String[40];
-        List<String[]> contacts = new ArrayList<>();
-        AddressBookFunctions.hashMapOfAddressBook.entrySet().stream().forEach(n->{
-                    strContacts[0] = n.getKey();
-                    strContacts[1] = n.getValue().toString();
-                    contacts.add(strContacts);
-                    csvWriter.writeAll(contacts);
-                }
-        );
+        fileWriter.write(str);
         fileWriter.close();
     }
     public static void readFromFile() throws IOException{
-        Scanner scanner = new Scanner(file);
-        scanner.useDelimiter(",");
-        while (scanner.hasNext()){
-            System.out.println(scanner.next());
-        }
-        scanner.close();
+        FileReader fileReader = new FileReader(file);
+        Object object = gson.toJson(fileReader,Object.class);
+        System.out.println(object);
+        fileReader.close();
     }
 }
